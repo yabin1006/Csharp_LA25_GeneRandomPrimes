9cc172f8acc983171f417485c37318e8191fd106
yabin1006 <yabin1006@yahoo.com>
2022-01-19 22:41:17 -0700

    Add project files.

---
+++ GeneratePrime.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LA25_GeneRandomPrimes_Robin
+{
+    class GeneratePrime
+    {
+        public Random rand = new Random();
+
+        public bool DeterminePrime(int number)
+        {
+            if (number == 0 || number == 1)
+                return false;
+            for (int i = 2; i < Math.Ceiling(Math.Sqrt(number)); i++)
+                if (number % i == 0)
+                    return false;
+            return true;
+        }
+
+        public int Primes()
+        {
+            int number;
+            //Random r = new Random();  //generate same number
+            number = rand.Next();
+            while (!DeterminePrime(number))
+                number++;
+            Console.Write($"{number}  ");
+            return number;
+        }
+
+        public List<int> Primes(int n)
+        {
+            List<int> primelist = new List<int>();
+            for (int i = 0; i < n; i++)
+                primelist.Add(Primes());
+            return primelist;
+        }
+
+        public List<int> Primes(int a, int b)  //a<=b
+        {
+            List<int> primelist = new List<int>();
+            int numcount = 0;
+            for (int i = a; i <= b; i++)
+                if (DeterminePrime(i))
+                {
+                    numcount++;
+                    primelist.Add(i);
+                    if(numcount%10 == 0)
+                        Console.Write($"{i}  \n");
+                    else Console.Write($"{i}  ");
+                }
+            if(primelist.Count == 0)
+                Console.WriteLine($"There is NO prime between {a} and {b}");
+            return primelist;
+        }
+
+        public void Primes(int c, int a, int b)
+        {
+            int numcount = 0;
+            List<int> primelist = new List<int>();
+            primelist = Primes(a, b);
+            int count = primelist.Count;
+            if (count > c)
+                primelist.RemoveRange(c, count - c);
+            else c = count;
+            for(int i=0; i<c; i++)
+            {
+                if (numcount++ == 0) Console.WriteLine($"\nThe first {c} exsit prime(s) is: ");
+                if(numcount%10 == 0)
+                    Console.Write($"{primelist[i]}  \n");
+                else Console.Write($"{primelist[i]}  ");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LA25_GeneRandomPrimes_Robin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GeneratePrime gp = new GeneratePrime();
+            Console.WriteLine("Generate Random Primes: ");
+            do
+            {
+                Console.WriteLine("\nGenerate Random Primes commands:\n -prime (Generate a random prime)\n -prime n (Generate n random primes)" +
+                    "\n -prime a b (Generate primes: a < primes <b; (a <= b))\n -prime c a b (Generate c primes: a < primes < b; (c>=1, a<=b)" +
+                    "\n -exit (Exit menu)");
+                Console.Write("Enter command (Above a,b, and c are integer number): ");
+                string command = Console.ReadLine().Trim().ToLower();
+                while (command.Contains("  "))
+                    command = command.Replace("  ", " ");
+                string[] strarr = command.Split(' ');
+
+                if (strarr[0] == "exit")
+                    break;
+                else if (strarr[0] == "prime" && strarr.Length == 1)
+                {
+                    Console.Write("\nRandom Primes: ");
+                    gp.Primes();
+                }
+                else if(strarr.Length == 2)
+                {
+                    Console.Write("\nRandom Primes: ");
+                    gp.Primes(int.Parse(strarr[1]));
+                }
+                else if (strarr.Length == 3)
+                {
+                    Console.Write("\nRandom Primes: ");
+                    gp.Primes(int.Parse(strarr[1]), int.Parse(strarr[2]));
+                }
+                else if (strarr.Length == 4)
+                {
+                    Console.Write("\nRandom Primes: ");
+                    gp.Primes(int.Parse(strarr[1]), int.Parse(strarr[2]), int.Parse(strarr[3]));
+                }
+                else Console.WriteLine("Entered command is NOT valid, try again.");
+            } while (true);
+        }
+    }
+}
